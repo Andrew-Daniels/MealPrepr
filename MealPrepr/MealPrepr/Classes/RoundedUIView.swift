4860f90f7071fcdf50af9ef0465eb879bc5513b4
Andrew Daniels <skaterphreak@gmail.com>
2018-12-04 18:53:45 -0600

    initial commit

---
+++ MealPrepr/MealPrepr/Classes/RoundedUIView.swift
@@ -0,0 +1,38 @@
+//
+//  RoundedUIView.swift
+//  MealPrepr
+//
+//  Created by Andrew Daniels on 12/4/18.
+//  Copyright © 2018 Andrew Daniels. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedUIView: UIView {
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    @IBInspectable
+    public var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    public var effect: UIBlurEffectStyle = .dark {
+        didSet {
+            let e = UIBlurEffect(style: self.effect)
+        
+            let v = UIVisualEffectView(effect: e)
+            self.addSubview(v)
+        }
+    }
+}
+
